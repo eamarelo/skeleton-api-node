042cb15846880f5787ee59ed01a792054a73123e
Elies AMARELO <eamarelo@hotmail.frfr>
2022-07-04 12:28:32 +0200

    [ADD] implements cretaeIngredients controller in route and in server

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -5,16 +5,24 @@ const GetMenus = require("./menus/get.js")
 const GetById = require("./menus/byId.js")
 const DestroyById = require("./menus/destroy.js")
 
+const CreateIngredients = require("./ingredients/create.js")
+
+
 module.exports = {
   //UNCOMMENT BELOW TO TEST
 
   // example: {
   //   ExampleController
-  // },
+
+  // }
+
   menus: {
     CreateMenus,
     GetMenus,
     GetById,
     DestroyById
+  },
+  ingredients: {
+    CreateIngredients
   }
 }
