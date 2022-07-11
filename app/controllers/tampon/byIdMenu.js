fa58e8ce9b932c3f7df119a32bb6c781e7b9889a
Elies AMARELO <eamarelo@hotmail.frfr>
2022-07-11 16:01:15 +0200

    [REF] delete useless console

--- app/controllers/tampon/byIdMenu.js
+++ app/controllers/tampon/byIdMenu.js
@@ -2,6 +2,7 @@
 // const fs = require("fs")
 const { sequelize } = require("../../db.js")
 const Tampons = require("../../models/tampon.js")
+const Ingredients = require("../../models/ingredients.js")
 
 module.exports = class TamponsByIdMenus {
 constructor(app) {
@@ -15,12 +16,26 @@ constructor(app) {
     async middleware() {
     this.app.get(`/tampon/byIdMenu`, async (req, res) => {
         try {
+            const arrayWhere = [];
+            const arrayResp = [];
+
             const menus = await Tampons.findAll({
                 where: {
                   idMenu: req.query.idMenu
                 }
-              });;
-
+              });
+              menus.map(menu => arrayWhere.push(menu.dataValues.idIngrédients))
+              console.log(arrayWhere)
+              const ingredients = await Ingredients.findAll({
+                where: {
+                  id: arrayWhere
+                }
+              });
+              ingredients.map(menu => arrayResp.push(menu.dataValues))
+              return res.status(200).json({
+                code: 200,
+                data: arrayResp
+            })
         } catch (e) {
             console.error(`[ERROR] menus/getAll -> ${e}`)
             return res.status(400).json({
