f582b6b7727c1aded5d31b0e70dcf58196cc7d9f
Elies AMARELO <eamarelo@hotmail.frfr>
2022-10-17 17:15:21 +0200

    [3350786655] Refactor - producteur dechets models

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -1,8 +1,12 @@
 
 const ExampleController = require("./example/index.js")
+const GetProducteurDechet = require("./producteurDechet/getProducteurDechet.js")
 
 module.exports = {
   example: {
     ExampleController
+  },
+  producteurDechets: {
+    GetProducteurDechet
   }
 }
--- app/db.js
+++ app/db.js
@@ -1,29 +1,22 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const sql = require('mssql')
+const { Sequelize } = require('sequelize');
 
-const config = { 
-  user: 'eamarelo',
-  password: "Gandia2021",
-  // local
-  server: '172.20.69.4', // You can use 'localhost\\instance' to connect to named instance
-  // ikoula
- //  server: '193.56.15.196'
-  database: 'BIBOARD',
-  driver: 'tedious',
-  options: {
-       encrypt: false // Use this if you're on Windows Azure
+const connectNewProd = new Sequelize('New_Prod', 'eamarelo', 'Gandia2021', {
+  dialect: 'mssql',
+  host: 'green-biboard',
+  dialectOptions: {
+    options: {
+      instanceName: 'BIPREPROD'
+    }
   }
-}
+})
 
-const poolPromise = new sql.ConnectionPool(config)
-  .connect()
-  .then(pool => {
-    console.log('Connected to MSSQL')
-    return pool
-  })
-  .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
-
-module.exports = {
-  sql, poolPromise
-}
+const connectAquarys = new Sequelize('Aquarys', 'eamarelo', 'Gandia2021', {
+  dialect: 'mssql',
+  host: 'green-biboard',
+  dialectOptions: {
+    options: {
+      instanceName: 'BIPREPROD'
+    }
+  }
+})
+module.exports = {connectNewProd, connectAquarys}
